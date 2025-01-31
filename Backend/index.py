cd3103ba58bff2a9c7fbd72898d32bb56503d50a
Subhan Asghar <subhanasghar16@gmail.com>
2025-01-31 14:56:41 +0500

    api/mcq route

--- Backend/index.py
+++ Backend/index.py
@@ -1,12 +1,16 @@
-from flask import Flask
-
+from flask import Flask, request,jsonify
+from mcq_agent import mcq_return
 app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+@app.route("/api/mcq",methods=['POST'])
 def fun():
-
-    return "<p>Hello, World!</p>" 
-
+    try:
+        data=request.json;
+        info=data.get('info');
+        result=mcq_return(info);
+        return jsonify({"message": f"{result}"}), 200
+    except:
+        return jsonify({"message": "Error"}),400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
